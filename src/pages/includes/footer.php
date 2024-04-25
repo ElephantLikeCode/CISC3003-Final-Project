42dde03ecaf20fdaef0dab5f60d9e542531c34a9
VictorLee07 <157529366+VictorLee07@users.noreply.github.com>
2024-04-26 04:57:25 +0800

    Add files via upload

---
+++ src/pages/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="center">
+        <h4>CISC3003-PAIR-03</h4>
+    </div>
+</footer>
---
+++ src/pages/includes/header.php
@@ -0,0 +1,11 @@
+<nav class="nav">
+    <a href="menu.php">
+        <img id="banner" src="images/banner.png" alt="banner">
+    </a>
+    <ul id="topMenu">
+        <li><a>News</a></li>
+        <li><a>Booking</a></li>
+        <li><a href="order.php">Order</a></li>
+        <li><a href="login.html">Login</a></li>
+    </ul>
+</nav>
