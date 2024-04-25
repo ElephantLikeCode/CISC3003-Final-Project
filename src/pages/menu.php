e0bfbdf389e2a53a82b41aa26a0ad3ad086cb134
VictorLee07 <157529366+VictorLee07@users.noreply.github.com>
2024-04-26 04:54:56 +0800

    Add files via upload

---
+++ src/pages/menu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Elephant Fitting Club | Stronger than elephant!</title>
+    <link href="style/public.css" type="text/css" rel="stylesheet">
+    <link href="style/menu.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<?php include_once "includes/header.php";?>
+    <section>
+    <div id='floatText'>
+        <img  src="images/hero-text.png" alt="text" style="width: 30vw;filter: opacity(100%)">
+    </div>
+    <img id="hero" src="images/hero.jpg" alt="hero">
+    </section>
+    <section>
+        <div id="intro" class="row center" >
+            <img src="images/fittness.jpg" style="height: 50vh;margin: 5%">
+            <div class="col center" style="max-width: 60vw">
+                <h1 class="xxxLarge bold" style="margin-bottom: 5%;color: #9B5F5F">Fitness, a way of life</h1>
+                <h1><i>Fitness, a way of life, transcends the boundaries of mere physical exercise and becomes a holistic approach to well-being.</i>
+                </h1>
+                <br>
+                <h1><i>It encompasses not only the act of engaging in regular physical activity but also the cultivation of healthy habits, both mentally and emotionally. </i></h1>
+                <br>
+                <h1><i> By embracing fitness as a way of life, individuals embark on a transformative journey that nurtures their bodies, sharpens their minds, and uplifts their spirits.</i></h1>
+            </div>
+        </div>
+    </section>
+</body>
+    <?php include_once "includes/footer.php";?>
+</html>
