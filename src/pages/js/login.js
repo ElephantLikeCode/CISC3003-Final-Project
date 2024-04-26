31b10d95268b74812f7e928ed91dc163b9e6ed1d
DEMAXICHEN123 <166804071+DEMAXICHEN123@users.noreply.github.com>
2024-04-26 22:58:25 +0800

    connect login to the database

---
+++ src/pages/js/login.js
@@ -0,0 +1,50 @@
+const signInBtn=document.querySelector('#signIn')
+const signUpBtn=document.querySelector('#signUp')
+const container=document.querySelector('.container')
+const container_signin=document.querySelector('#sign_in')
+const container_signup=document.querySelector("#sign_up")
+const signUp=document.querySelector('.signUp')
+const signIn=document.querySelector('.signIn')
+signUp.addEventListener('click',()=>{
+    container_signin.classList.add("none")
+    container_signup.classList.remove("none")
+
+})
+signIn.addEventListener('click',()=>{
+    container_signin.classList.remove("none")
+    container_signup.classList.add("none")
+
+})
+signInBtn.addEventListener("click",()=>{
+    container.classList.remove("panel-active")
+})
+signUpBtn.addEventListener("click",()=>{
+    container.classList.add("panel-active")
+})
+
+const forms = document.querySelectorAll('form');
+forms.forEach((form) => {
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // 阻止表单的默认提交行为
+
+        const formData = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'login.php');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                const loginResult = response.loginResult;
+
+                // 在 HTML 页面中进行显示
+                if (loginResult === 2) {
+                    alert('Email already registered.');
+                } else if (loginResult === 3) {
+                    alert('Wrong account password.');
+                } else {
+                    alert('Email not registered.');
+                }
+            }
+        };
+        xhr.send(formData);
+    });
+});
---
+++ src/pages/login.php
@@ -0,0 +1,55 @@
+<?php
+$servername = "localhost";
+$db_username = "root";
+$db_password = "20140304Aa";
+$dbname = "login_system";
+
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+$loginResult=1;
+
+if (count($_POST) == 3){
+    $user_name=$_POST["User"];
+    $password=$_POST["Password"];
+    $Email=$_POST["Email"];
+
+    $signup = "INSERT INTO user (Email, user_name, password) VALUES ('".$Email."', '".$user_name."','".$password."')";
+
+    if (mysqli_query($conn, $signup)) {
+        $accountInfo=$_POST;
+        setcookie('accountInfo', json_encode($accountInfo), time() + 3600, '/');
+        header('Location: menu.php');
+        exit();//注册成功，将账户信息存入cookie，导向主页面
+    } else {
+        $loginResult = 2;//邮箱已注册
+    }
+
+}
+else{
+
+    $password=$_POST["Password"];
+    $Email=$_POST["Email"];
+
+    $signup = "SELECT user_name,password from user where Email='".$Email."'";
+    $result = $conn->query($signup);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $storedPassword = $row["password"];
+        $user_name=$row["user_name"];
+
+        if ($storedPassword == $password){
+            $accountInfo = $row;
+            setcookie('accountInfo', json_encode($accountInfo), time() + 3600, '/');
+            header('Location: menu.php');
+            exit();//登录成功，储存cookie，导向主页面
+        }else{
+            $loginResult=3;//密码错误
+        }
+    }
+    else{
+        $loginResult=4;//邮箱未注册
+    }
+
+}
+echo json_encode(['loginResult' => $loginResult]);
+?>
