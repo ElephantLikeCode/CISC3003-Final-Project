3dca89b9c6acfa491912c238ed36275cd5dd657a
VictorLee07 <157529366+VictorLee07@users.noreply.github.com>
2024-04-30 21:21:20 +0800

    Add files via upload

---
+++ src/pages/cart.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style/public.css" type="text/css" rel="stylesheet">
+    <link href="style/cart.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Elephant Fitting Club | Shopping Cart</title>
+</head>
+<body>
+    <?php include "inlucdes/header.php"?>
+    <?php include "inlucdes/footer.php"?>
+</body>
+</html>
