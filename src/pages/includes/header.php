788a10827562b64a406bd6c18a81c7daaa8880e8
VictorLee07 <157529366+VictorLee07@users.noreply.github.com>
2024-04-30 21:20:52 +0800

    Add files via upload

--- src/pages/includes/header.php
+++ src/pages/includes/header.php
@@ -8,7 +8,7 @@
             <span class="hide-for-mobile"><a href="#">News</a></span>
             <span class="hide-for-mobile"><a href="#">Booking</a></span>
             <span class="hide-for-mobile"><a href="order.php">Order</a></span>
-            <span><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="hide-for-mobile">Cart</span></a></span>
+            <span><a href="cart.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="hide-for-mobile">Cart</span></a></span>
             <span><a href="login.html"><i class="fa fa-user-circle" aria-hidden="true"></i><span class="hide-for-mobile">Login</span></a></span>
             
             <a href="#" id="btnHamburger" class="header-toggle hide-for-desktop">
