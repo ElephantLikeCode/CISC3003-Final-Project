ecb5c5f8694c69654b99972834175e13578b0cb7
Ted <tanum2218@gmail.com>
2024-05-01 19:26:23 +0800

    Change the file name
    I change the name of the file "menu.php" to "index.php". Since that is our website homepage.

--- src/pages/login.php
+++ src/pages/login.php
@@ -18,7 +18,7 @@ if (count($_POST) == 3){
     if (mysqli_query($conn, $signup)) {
         $accountInfo=$_POST;
         setcookie('accountInfo', json_encode($accountInfo), time() + 3600, '/');
-        header('Location: menu.php');
+        header('Location: index.php');
         exit();//注册成功，将账户信息存入cookie，导向主页面
     } else {
         $loginResult = 2;//邮箱已注册
@@ -40,7 +40,7 @@ else{
         if ($storedPassword == $password){
             $accountInfo = $row;
             setcookie('accountInfo', json_encode($accountInfo), time() + 3600, '/');
-            header('Location: menu.php');
+            header('Location: index.php');
             exit();//登录成功，储存cookie，导向主页面
         }else{
             $loginResult=3;//密码错误
