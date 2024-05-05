281ba9d91915ed2dd27c83abebbff93c6094d4b5
Aquila1231101 <2260708834@qq.com>
2024-05-05 23:19:26 +0800

    bookingResult

--- src/pages/bookingResult.php
+++ src/pages/bookingResult.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style/order.css">
+    <link rel="stylesheet" href="style/bookResult.css">
     <title>Elephant Fitting Club | Stronger than elephant!</title>
 </head>
 <body>
@@ -16,16 +17,21 @@
     <div style="display: flex; justify-content: center;height: 70vh;align-items: center;flex-flow: column">
     <?php
     session_start();
-    if($_SESSION['result']==='false'){
+    if(!true){
         echo "<h1 style='font-size: xxx-large'>The time slot is already booked!</h1>
                 <button onclick='window.open(`addBooking.php?item=meal-title`)' class='beautiful-button' style='margin-top: 5vh;font-size: x-large;padding: 2vh'>Return to book another!</button>";
     }
     else{
-        $startDate=$_SESSION['startDate'];
+        /*$startDate=$_SESSION['startDate'];
         $startTime=$_SESSION['startTime'];
         $endDate=$_SESSION['endDate'];
-        $endTime=$_SESSION['endTime'];
-        echo "<h1>Booking Successes</h1>";
+        $endTime=$_SESSION['endTime'];*/
+        $startDate=1;
+        $startTime=2;
+        $endDate=3;
+        $endTime=4;
+        echo "<h1 style='font-size: xxx-large'>Booking Successes!</h1><br>
+                <h1 style='font-size: xxx-large'>Time to Do Some Exercise!</h1><br><br><br>";
         echo "
 <table>
     <tr>
@@ -41,7 +47,8 @@
     <td>$endDate</td>
     <td>$endTime</td>
 </tr>
-</table>";
+</table>
+<button onclick='window.open(`addBooking.php?item=meal-title`)' class='beautiful-button' style='margin-top: 5vh;font-size: x-large;padding: 2vh'>Return to book another!</button>";
         session_destroy();
     }
     ?>
