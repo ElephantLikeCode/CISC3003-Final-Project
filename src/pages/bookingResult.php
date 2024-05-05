01fb20802fb981f44c7e0abf997e56f099d42e46
Aquila1231101 <2260708834@qq.com>
2024-05-05 23:02:04 +0800

    bookingResult

--- src/pages/bookingResult.php
+++ src/pages/bookingResult.php
@@ -1,8 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style/public.css" type="text/css" rel="stylesheet">
+    <link href="style/menu.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Maven+Pro:wght@400..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/order.css">
+    <title>Elephant Fitting Club | Stronger than elephant!</title>
+</head>
+<body>
+<?php include "includes/header.php";?>
+<section>
+    <div style="display: flex; justify-content: center;height: 70vh;align-items: center;flex-flow: column">
+    <?php
     session_start();
-    echo $_SESSION['result'];
     if($_SESSION['result']==='false'){
-        echo "<h1>The time slot is already booked!</h1>";
+        echo "<h1 style='font-size: xx-large'>The time slot is already booked!</h1>
+                <button onclick='window.open(`addBooking.php?item=meal-title`)' class='beautiful-button' style='margin-top: 5vh;font-size: x-large;padding: 2vh'>Return to book another!</button>";
     }
     else{
         $startDate=$_SESSION['startDate'];
@@ -28,3 +44,9 @@
 </table>";
         session_destroy();
     }
+    ?>
+    </div>
+</section>
+</body>
+<?php include "includes/footer.php";?>
+</html>
